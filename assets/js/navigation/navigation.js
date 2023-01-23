16b7b299dfd68a440371ea94d609be6c15ca688f
flyinglawnmower <43915072+puggen1@users.noreply.github.com>
2023-01-23 12:06:27 +0100

    ver 1 of navigation done, might need better animation

--- assets/js/navigation/navigation.js
+++ assets/js/navigation/navigation.js
@@ -1,7 +1,31 @@
 let navButton = document.querySelector("#top label");
 let sideNav = document.querySelector("#navigation");
-navButton.addEventListener("click", (e)=>{
-    sideNav.style.animation ="navIn 2s linear infinite";
-    
-})
+let navContent = document.querySelector("#navContent");
+let back = document.querySelector("#backNav");
+let allHidden = document.querySelectorAll(".hidden");
+navButton.addEventListener("click", (e) => {
+  sideNav.style.width = "100%";
+  navContent.style.width = "300px";
+  allHidden.forEach((ele) => {
+    ele.classList.remove("hidden");
+  });
+});
 console.log(navButton);
+back.addEventListener("click", (e) => {
+  sideNav.style.width = "0px";
+  navContent.style.width = "0px";
+  allHidden.forEach((ele) => {
+    ele.classList.add("hidden");
+  });
+});
+sideNav.addEventListener("click", (e) => {
+  console.log(e);
+  if (e.target.id !== "navigation") {
+    return;
+  }
+  sideNav.style.width = "0px";
+  navContent.style.width = "0px";
+  allHidden.forEach((ele) => {
+    ele.classList.add("hidden");
+  });
+});
