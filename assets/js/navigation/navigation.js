84a12e9d6badf88d40e33b389ac2e9353a651f32
flyinglawnmower <43915072+puggen1@users.noreply.github.com>
2023-01-23 16:16:57 +0100

    removed nav item, cloned to every page

--- assets/js/navigation/navigation.js
+++ assets/js/navigation/navigation.js
@@ -1,31 +1,40 @@
+//targeting all the needed content and buttons
 let navButton = document.querySelector("#top label");
 let sideNav = document.querySelector("#navigation");
 let navContent = document.querySelector("#navContent");
 let back = document.querySelector("#backNav");
 let allHidden = document.querySelectorAll(".hidden");
+
+//when navbutton is clicked the menu will show
 navButton.addEventListener("click", (e) => {
-  sideNav.style.width = "100%";
-  navContent.style.width = "300px";
-  allHidden.forEach((ele) => {
-    ele.classList.remove("hidden");
-  });
+  sideNav.classList.toggle("showNavParent");
+  navContent.classList.toggle("showNav");
+  //a simple working delay for the content inside the nav, so it wont show before the transistion is done (did this instead of alot of animation)
+  setTimeout(() => {
+    allHidden.forEach((ele) => {
+      ele.classList.remove("hidden");
+    });
+  }, 300);
 });
-console.log(navButton);
+
+//both of these eventlisters closes the nav menu, either if you click the button, or outside the nav on bigger screens
 back.addEventListener("click", (e) => {
-  sideNav.style.width = "0px";
-  navContent.style.width = "0px";
-  allHidden.forEach((ele) => {
-    ele.classList.add("hidden");
-  });
+  closeNavigation("back");
 });
 sideNav.addEventListener("click", (e) => {
+  closeNavigation(e);
+});
+
+function closeNavigation(e) {
   console.log(e);
-  if (e.target.id !== "navigation") {
+  //since the back button have content inside, i had to send a string trough so it does not take icon's atributes and dont run
+  if (e === "back" || !(e.target.id !== "navigation")) {
+    sideNav.classList.toggle("showNavParent");
+    navContent.classList.toggle("showNav");
+    allHidden.forEach((ele) => {
+      ele.classList.add("hidden");
+    });
+  } else {
     return;
   }
-  sideNav.style.width = "0px";
-  navContent.style.width = "0px";
-  allHidden.forEach((ele) => {
-    ele.classList.add("hidden");
-  });
-});
+}
